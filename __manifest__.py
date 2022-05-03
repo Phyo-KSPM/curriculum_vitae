39f9ac06015901585ec1078a67a43aec0fb44a10
Phyo-KSPM <67825821+Phyo-KSPM@users.noreply.github.com>
2022-05-03 14:16:36 +0630

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Curriculum Vitae",
+    'author': "Ko Phyo",
+
+    'website': "dream.fiberinternet@gmial.com",
+    'category': "CV Form",
+    'summary': """
+        Dream Eleven Internet Services Provider Myanmar
+    """,
+
+    'description': """
+        Dream Elevens Internet Services Provider
+            - Upgrade your life with Dream FTTH Services
+    """
+}
